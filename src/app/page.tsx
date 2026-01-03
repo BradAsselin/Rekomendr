af0b30bca8ee6cb29bc2e2ebb8f388e22a50ace7
BradAsselin <bradasselin@gmail.com>
2026-01-03 15:53:08 -0500

    Remove src/app so Next uses root app router

--- src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-import React from "react";
-import ResultsV4 from "../components/ResultsV4";
-
-export default function Home() {
-  return <ResultsV4 initialVertical="movies" autoRunVertical />;
-}
